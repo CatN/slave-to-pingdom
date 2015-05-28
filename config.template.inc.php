dfab5dafbc6d5ea662b642b47fd12b7f6c471a80
Ben Kennish <ben@kennish.net>
2015-05-28 15:32:30 +0100

    Initial commit

---
+++ config.template.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'slave-to-pingdom');
+define('DB_PASSWD', 'XXXXXXXXXXXXXXXXX');
+
+define('MAX_SECS_BEHIND_MASTER', 7200); // complain if we get more than this much behind master
+
