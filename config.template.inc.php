474f5605fab459c19733064cd59e7f21b562c256
Ben Kennish <ben@fubra.com>
2017-04-04 10:59:09 +0100

    Change default MAX_SECS_BEHIND_MASTER to 1hr from 2hr

--- config.template.inc.php
+++ config.template.inc.php
@@ -4,5 +4,5 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'slave-to-pingdom');
 define('DB_PASSWD', 'XXXXXXXXXXXXXXXXX');
 
-define('MAX_SECS_BEHIND_MASTER', 7200); // complain if we get more than this much behind master
+define('MAX_SECS_BEHIND_MASTER', 3600); // complain if we get more than this much behind master
 
